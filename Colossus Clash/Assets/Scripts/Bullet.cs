07e1f5d1f22796ab1420485364ed571db92446d6
Paul Schatz <prs5991@rit.edu>
2024-04-18 20:09:01 -0400

    bullet now dissapears after some time

--- Colossus Clash/Assets/Scripts/Bullet.cs
+++ Colossus Clash/Assets/Scripts/Bullet.cs
@@ -7,9 +7,20 @@ public class Bullet : MonoBehaviour
     public float speed = 20f;
     public int damage = 20;
     public Rigidbody2D rb; // Assign this in the inspector with the Rigidbody2D component attached to your bullet prefab.
+    public float timeAlive = 1.0f; //time the bullet should stay before being deleted
+    private float timePassed; //time the bullet has been alive, updated each frame
 
     void Start()
     {
         rb.velocity = transform.up * speed;
+        timePassed = 0;
+    }
+    private void Update()
+    {
+        timePassed+= Time.deltaTime;
+        if (timePassed > timeAlive)
+        {
+            Destroy(gameObject);
+        }
     }
 }
